ac2bf40ac7567fd573802608b2cd06862e1ee781
DavidNaderer03 <aocnaderer@gmail.com>
2024-02-24 16:53:47 +0100

    logic update

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const header = [
     {
         names: ["stefan", "julia"],
-        accuracy: 100
+        accuracy: () => alert("100")
     },
     {
         names: ["david", "sophia"],
-        accuracy: "Was glaubst du den"
+        accuracy: () => alert("Was glaubst du den")
     },
     {
         names: ["felix", "elli"],
-        accuracy: "eher weniger"
+        accuracy: () => alert("eher weniger")
     },
     {
         names: ["felix", "elleonora"],
-        accuracy: "eher weniger"
+        accuracy: () => alert("eher weniger")
+    },
+    {
+        names: ["felix", "felix"],
+        accuracy: () => location.href = "spinningfelix.html"
+    },
+    {
+        names: ["pages", "pages"],
+        accuracy: () => location.href = "pages.html"
+    },
+    {
+        names: ["stefan", "sam"],
+        accuracy: () => alert("als zweite Wahl")
     }
 ]
 
@@ -23,25 +35,19 @@ function calcLove() {
         found: false,
         count: 0
     };
-    if (names.every(element => element === "felix")) {
-        location.href = "spinningfelix.html";
-    } else if (names.every(x => x === 'pages')) {
-        location.href = "pages.html";
-    } else {
-        for (const element of header) {
-            for (const name of element.names) {
-                if (names.some(element => element === name)) {
-                    result.count++;
-                }
+    for (const element of header) {
+        for (const name of element.names) {
+            if (names.some(element => element === name)) {
+                result.count++;
             }
-            if (result.count === 2) {
-                alert(element.accuracy);
-                result.found = true;
-            }
-            result.count = 0;
         }
-        if (!result.found) {
-            alert(Math.round(Math.random() * 100));
+        if (result.count === 2) {
+            element.accuracy();
+            result.found = true;
         }
+        result.count = 0;
+    }
+    if (!result.found) {
+        alert(Math.round(Math.random() * 100));
     }
 }
